9d017981b9b98f25873f7a9e00aecd3ff9362cb6
Nathan Thomas <nathomas92@gmail.com>
2015-09-07 05:44:48 -0700

    Checks for results before trying to access them

--- utilities.py
+++ utilities.py
@@ -7,6 +7,8 @@ class Urban():
         req = requests.get(
                 "http://api.urbandictionary.com/v0/define", {'page': 1, 'term': term}
                 )
-        define = req.json()['list'][0]['definition'].replace('\r\n', ' ')
+        if len(req.json()['list']) == 0:
+            return "No results for {0}".format(term)
+        define = req.json()['list'][0]['definition']
         short = define[:define.rfind(" ", 0, length)]
         return short + "\n" + req.json()['list'][0]['permalink']
